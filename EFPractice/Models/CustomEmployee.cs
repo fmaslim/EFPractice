74e890f8cccc66688cfef3db048bfd64cccd1122
fmaslim <franky.maslim@yahoo.com>
2024-05-07 21:02:24 -0400

    Scaffolded DB entity DB models

---
+++ EFPractice/Models/CustomEmployee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFPractice.Models;
+
+public partial class CustomEmployee
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+}
